99ad82ccfb6e1d1063b285a24a202ae5ae932273
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-06-18 03:28:55 +0300

    done: display items

--- react-shop/src/App.js
+++ react-shop/src/App.js
@@ -1,10 +1,8 @@
 import { Header } from './components/Header';
 import { Shop } from './components/Shop';
 import { Footer } from './components/Footer';
-import { API_KEY } from './config';
 
 function App() {
-	console.log(API_KEY);
 	return (
 		<>
 			<Header />
--- react-shop/src/components/Shop.jsx
+++ react-shop/src/components/Shop.jsx
@@ -1,3 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { API_KEY, API_URL } from '../config';
+import { GoodsList } from './GoodsList';
+import { Preloader } from './Preloader';
+
 export function Shop(params) {
-	return <main className='container content'>ulala</main>;
+	const [goods, setGoods] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(function GetGoods() {
+		fetch(API_URL, {
+			headers: {
+				Authorization: API_KEY,
+			},
+		})
+			.then((r) => r.json())
+			.then((data) => setGoods(data));
+	}, []);
+
+	return (
+		<main className='container content'>
+			{loading ? <Preloader /> : <GoodsList goods={goods.shop} />}
+		</main>
+	);
 }
