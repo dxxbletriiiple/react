0e834413adb22516f7de46d4e43fb92dd1f5e206
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-10-29 14:37:31 +0300

    init: marvel-2 using ts

--- marvel-2/src/services/MarvelService.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-export class MarvelService {
-	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
-	_apiKey = process.env.APIKEY;
-}
-console.log(new MarvelService()._apiKey);
