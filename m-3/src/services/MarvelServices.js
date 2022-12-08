937074d58e7bf33bcd9d2a3057f5be7828250e6a
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-12-08 11:17:18 +0300

    fix: MS

--- m-3/src/services/MarvelServices.js
+++ m-3/src/services/MarvelServices.js
@@ -10,34 +10,33 @@ export class MarvelService {
 		if (!res.ok) {
 			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
 		}
-
 		return await res.json();
 	};
 
-	getAllResources = () => {
-		return this.getResource(
+	getAllResources = async () => {
+		const res = await this.getResource(
 			`${this._baseUrl}characters?apikey=${this._apiKey}&ts=${this._TS}&hash=${this._hash}`,
 		);
+		return res.data.results.map(this._transformCharacter);
 	};
 	getCharacter = async (id) => {
-		const res = this.getResource(
-			`${this._baseUrl}characters${id}?apikey=${this._apiKey}&ts=${this._TS}&hash=${this._hash}`,
+		const res = await this.getResource(
+			`${this._baseUrl}characters/${id}?apikey=${this._apiKey}&ts=${this._TS}&hash=${this._hash}`,
 		);
 		return this._transformCharacter(res.data.results[0]);
 	};
 
-	_transformCharacter = ({ name, description, thumbnail, urls, id, comics2, wiki }) => {
+	_transformCharacter = ({ name, description, thumbnail, urls, id, comics }) => {
 		return {
+			id,
 			name,
 			description: description
-				? `${description.slice(0, 210)}...`
+				? `${description.slice(0, 230)}...`
 				: `There is no description about ${name}`,
 			thumbnail: `${thumbnail?.path}.${thumbnail?.extension}`,
 			homepage: urls[0].url,
 			wiki: urls[1].url,
-			id: id,
-			comics: comics2.items,
-			urls: [],
+			comics: comics.items,
 		};
 	};
 }
