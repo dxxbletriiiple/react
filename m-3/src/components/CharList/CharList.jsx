99046c56525ba10dbfff8deec4d407ff7cbeae73
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-12-18 17:06:03 +0300

    done: charlist pagination; todo: charlist error

--- m-3/src/components/CharList/CharList.jsx
+++ m-3/src/components/CharList/CharList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useLayoutEffect } from 'react';
 import { MarvelService } from '../../services/MarvelServices';
 import { CharListItem } from '../CharListItem/CharListItem';
 import './charList.scss';
@@ -7,27 +7,28 @@ export const CharList = ({ onCharSelected }) => {
 	const [list, setList] = useState([]);
 	const [offset, setOffset] = useState(0);
 	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(false);
+	const [end, setEnd] = useState(false);
+	// const [error, setError] = useState(false);
 	const ms = new MarvelService();
 
-	useEffect(() => {
-		onCharListLoaded(offset);
+	useLayoutEffect(() => {
+		onRequest();
 		// eslint-disable-next-line
 	}, []);
 
-	const onCharListLoaded = (offset2) => {
-		ms.getAllResources(offset2)
-			.then((res) => {
-				setIsLoading(true);
-				setList([...list, ...res]);
-			})
-			.then(() => setIsLoading(false))
-			.catch(() => setError(true));
+	const onLoading = () => {
+		setOffset(offset + 9);
 	};
 
-	const handleClick = () => {
-		setOffset(offset + 9);
-		onCharListLoaded(offset + 9);
+	const onRequest = (offset) => {
+		onLoading();
+		setIsLoading(true);
+		ms.getAllResources(offset).then((res) => {
+			if (res.length === 0) return setEnd(true);
+			setList([...list, ...res]);
+			setIsLoading(false);
+		});
+		// .catch(() => setError(true));
 	};
 
 	return (
@@ -40,7 +41,8 @@ export const CharList = ({ onCharSelected }) => {
 			<button
 				className='button button__main button__long'
 				disabled={isLoading}
-				onClick={handleClick}
+				onClick={() => onRequest(offset)}
+				style={{ display: end ? 'none' : 'block' }}
 			>
 				<div className='inner'>load more</div>
 			</button>
