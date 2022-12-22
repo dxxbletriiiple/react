21322eb47f1324472d25a74e6999fd497232512c
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-12-22 19:41:53 +0300

    add: select char with keyboard

--- m-3/src/components/CharListItem/CharListItem.jsx
+++ m-3/src/components/CharListItem/CharListItem.jsx
@@ -2,8 +2,18 @@ export const CharListItem = ({ id, name, thumbnail, onCharSelected }) => {
 	const imgStyle = thumbnail?.includes('image_not_available')
 		? { objectFit: 'contain' }
 		: { objectFit: 'cover' };
+
+	const handleSelect = (e) => {
+		return e.key === ' ' || e.key === 'Enter' || e.type === 'click' ? onCharSelected(id) : null;
+	};
 	return (
-		<li className='char__item' key={id + name} onClick={() => onCharSelected(id)}>
+		<li
+			className='char__item'
+			key={id + name}
+			tabIndex={0}
+			onClick={handleSelect}
+			onKeyDown={handleSelect}
+		>
 			<img src={thumbnail} alt={name} style={imgStyle} />
 			<div className='char__name'>{name}</div>
 		</li>
