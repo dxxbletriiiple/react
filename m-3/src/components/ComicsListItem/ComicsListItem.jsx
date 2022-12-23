71d94b01076dfcbf15ff10bdb2a7f595e885aedb
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-12-23 21:28:14 +0300

    add: routing

--- m-3/src/components/ComicsListItem/ComicsListItem.jsx
+++ m-3/src/components/ComicsListItem/ComicsListItem.jsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom';
 import './ComicsListItem.scss';
 
 export const ComicsListItem = ({ id, title, thumbnail, price }) => {
 	const st = { objectFit: thumbnail?.includes('image_not_available') ? 'contain' : '' };
 	return (
 		<li className='comics__item' key={id + title} tabIndex={0}>
-			<a href='#!'>
+			<Link to={`${id}`}>
 				<img src={thumbnail} style={st} alt='ultimate war' className='comics__item-img' />
 				<div className='comics__item-name'>{title}</div>
 				<div className='comics__item-price'>{price}</div>
-			</a>
+			</Link>
 		</li>
 	);
 };
