c151dd18a828e8105572b6468c0f4d5244908afc
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-01-17 14:40:08 +0300

    done: collections

--- shopi/src/components/Brands/Brands.jsx
+++ shopi/src/components/Brands/Brands.jsx
@@ -7,42 +7,42 @@ export const Brands = () => {
 			<ul className='brands_list'>
 				<li>
 					<a href='!'>
-						<svg class='icon' role='img' style={{ width: '70px', height: '30px' }}>
+						<svg className='icon' role='img' style={{ width: '70px', height: '30px' }}>
 							<use xlinkHref={`${svg}#prada`}></use>
 						</svg>
 					</a>
 				</li>
 				<li>
 					<a href='!'>
-						<svg class='icon' role='img' style={{ width: '100px', height: '30px' }}>
+						<svg className='icon' role='img' style={{ width: '100px', height: '30px' }}>
 							<use xlinkHref={`${svg}#burberry`}></use>
 						</svg>
 					</a>
 				</li>
 				<li>
 					<a href='!'>
-						<svg class='icon' role='img' style={{ width: '54px', height: '30px' }}>
+						<svg className='icon' role='img' style={{ width: '54px', height: '30px' }}>
 							<use xlinkHref={`${svg}#boss`}></use>
 						</svg>
 					</a>
 				</li>
 				<li>
 					<a href='!'>
-						<svg class='icon' role='img' style={{ width: '72px', height: '30px' }}>
+						<svg className='icon' role='img' style={{ width: '72px', height: '30px' }}>
 							<use xlinkHref={`${svg}#cartier`}></use>
 						</svg>
 					</a>
 				</li>
 				<li>
 					<a href='!'>
-						<svg class='icon' role='img' style={{ width: '96px', height: '30px' }}>
+						<svg className='icon' role='img' style={{ width: '96px', height: '30px' }}>
 							<use xlinkHref={`${svg}#gucci`}></use>
 						</svg>
 					</a>
 				</li>
 				<li>
 					<a href='!'>
-						<svg class='icon' role='img' style={{ width: '96px', height: '30px' }}>
+						<svg className='icon' role='img' style={{ width: '96px', height: '30px' }}>
 							<use xlinkHref={`${svg}#tiffany`}></use>
 						</svg>
 					</a>
--- shopi/src/components/Collections/Collections.jsx
+++ shopi/src/components/Collections/Collections.jsx
@@ -1,9 +1,16 @@
+import img1 from '../../assets/collections_1.jpg';
+import img2 from '../../assets/collections_2.jpg';
 import './Collections.scss';
 
 export const Collections = () => {
 	return (
 		<div className='collections'>
 			<h2 className='collections_header'>Collections</h2>
+			<div className='content'>
+				<img className='october' src={img1} alt='october' />
+				<img className='autumn' src={img2} alt='autumn' />
+				<video src='https://cdn.coverr.co/videos/coverr-browsing-in-a-fashion-store-4162/1080p.mp4'></video>
+			</div>
 		</div>
 	);
 };
