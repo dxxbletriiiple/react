63d21838cf607ebc922d15af6f5d8e1d30f3633c
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-01-20 17:57:05 +0300

    add: spinner

---
+++ star-wars/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import './Spinner.scss';
+
+export const Spinner = () => {
+	return (
+		<div className='loadingio-spinner-eclipse-ftgj832v907'>
+			<div className='ldio-z2ivfzoxns'>
+				<div></div>
+			</div>
+		</div>
+	);
+};
