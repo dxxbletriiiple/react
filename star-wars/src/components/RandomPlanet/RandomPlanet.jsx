8f626f5c8bd6a48962024480e0f97585f6e2439b
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-01-20 17:58:03 +0300

    edit: random planet

--- star-wars/src/components/RandomPlanet/RandomPlanet.jsx
+++ star-wars/src/components/RandomPlanet/RandomPlanet.jsx
@@ -1,29 +1,63 @@
+import { useState, useEffect } from 'react';
+import SwapiService from '../../services/SwapiServise';
+import { Spinner } from '../Spinner/Spinner';
+import { ErrorIndicator } from '../ErrorIndicator/ErrorIndicator';
 import './RandomPlanet.scss';
 
 export const RandomPlanet = () => {
+	const [planet, setPlanet] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+
+	const sp = new SwapiService();
+
+	useEffect(() => {
+		const id = Math.floor(Math.random() * 60);
+		sp.getPlanet(id)
+			.then((res) => {
+				setLoading(false);
+				setPlanet(res);
+			})
+			.catch((e) => {
+				setLoading(false);
+				setError(true);
+				console.log(e);
+			});
+	}, []);
+
 	return (
-		<div className='random-planet jumbotron rounded'>
+		<div className='random-planet jumbotron rounded mb-3'>
+			{loading ? <Spinner /> : null}
+			{error ? <ErrorIndicator /> : null}
+			{planet ? <View {...planet} /> : null}
+		</div>
+	);
+};
+
+const View = ({ id, name, population, rotationPeriod, diameter }) => {
+	return (
+		<>
 			<img
 				className='planet-image'
-				src='https://starwars-visualguide.com/assets/img/planets/5.jpg'
+				src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
 			/>
 			<div>
-				<h4>Planet Name</h4>
+				<h4>{name}</h4>
 				<ul className='list-group list-group-flush'>
 					<li className='list-group-item'>
 						<span className='term'>Population</span>
-						<span>123124</span>
+						<span>{population}</span>
 					</li>
 					<li className='list-group-item'>
 						<span className='term'>Rotation Period</span>
-						<span>43</span>
+						<span>{rotationPeriod}</span>
 					</li>
 					<li className='list-group-item'>
 						<span className='term'>Diameter</span>
-						<span>100</span>
+						<span>{diameter}</span>
 					</li>
 				</ul>
 			</div>
-		</div>
+		</>
 	);
 };
