1f15e3839d9c9c054a8adcbb12ca03a535be536f
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-01-22 23:38:52 +0300

    add: context; fix: text-align

--- star-wars/src/components/PersonDetails/PersonDetails.jsx
+++ star-wars/src/components/PersonDetails/PersonDetails.jsx
@@ -1,27 +1,29 @@
+import { useContext } from 'react';
+import { context as data } from '../../context/context';
 import './PersonDetails.scss';
 
 export const PersonDetails = () => {
+	const {
+		selectedPerson: { id, name, gender, eyeColor, birthYear },
+	} = useContext(data);
 	return (
 		<div className='person-details card'>
-			<img
-				className='person-image'
-				src='https://starwars-visualguide.com/assets/img/characters/3.jpg'
-			/>
+			<img className='person-image' src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
 
 			<div className='card-body'>
-				<h4>R2-D2</h4>
+				<h4>{name}</h4>
 				<ul className='list-group list-group-flush'>
 					<li className='list-group-item'>
 						<span className='term'>Gender</span>
-						<span>male</span>
+						<span>{gender}</span>
 					</li>
 					<li className='list-group-item'>
 						<span className='term'>Birth Year</span>
-						<span>43</span>
+						<span>{birthYear}</span>
 					</li>
 					<li className='list-group-item'>
 						<span className='term'>Eye Color</span>
-						<span>red</span>
+						<span>{eyeColor}</span>
 					</li>
 				</ul>
 			</div>
