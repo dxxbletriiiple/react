d82c28d4bff20b26dda9573284ba5c3f01f2b7eb
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-01-23 02:12:26 +0300

    edit: delete default person in contextc

--- star-wars/src/context/context.js
+++ star-wars/src/context/context.js
@@ -1,12 +1,6 @@
 import { createContext } from 'react';
 
 export const context = createContext({
-	selectedPerson: {
-		id: '1',
-		name: 'Luke Skywalker',
-		gender: 'male',
-		birthYear: '19BBY',
-		eyeColor: 'blue',
-	},
+	selectedPerson: {},
 	onPersonSelected() {},
 });
