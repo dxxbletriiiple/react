cd83fedbc4efe6647f09490679d52225c3f36b36
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-01-29 15:15:30 +0300

    add: mock

---
+++ devfinder/src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+	id: 44002603,
+	login: 'dxxbletriiiple',
+	avatar: 'https://avatars.githubusercontent.com/u/44002603?v=4',
+	name: 'dxxbletriiiple',
+	company: 'null',
+	blog: 'dxxbletriiiple.ru',
+	location: 'Innopolis, Russia',
+	bio: 'Frontend developer',
+	twitter: 'dxxbletriiiple',
+	repos: 60,
+	followers: 14,
+	following: 24,
+	created: '2018-10-09T17:55:08Z',
+};
